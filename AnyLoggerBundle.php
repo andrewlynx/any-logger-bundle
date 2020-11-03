033d1e146bc2ee2f3815d0311bdcd7756080de80
Andrew M <a.melnik.work@gmail.com>
2020-11-03 18:00:21 +0200

    init

---
+++ AnyLoggerBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Andrewlynx\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AnyLoggerBundle extends Bundle
+{
+    public function __construct()
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+### Simple logger bundle for Symfony
